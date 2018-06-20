7eecac74668a8895cb6de01b4c8f5789c3a0e0e8
Michelle Kruk <kruk.mich@gmail.com>
2018-06-20 15:41:13 +0000

    automatically backed up by learn

---
+++ hashes.rb
@@ -0,0 +1,7 @@
+#hashes are like dictionaries --> they hold keys which are like words in a dictionary 
+#and each of these keys has a value which is like the definition of the word 
+#different from arrays, because in arrays, each element has an index (aka position) that is a number
+
+
+
+celeb_crush = {}
